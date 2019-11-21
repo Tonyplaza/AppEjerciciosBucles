3a2d69fe975342b8c1077ee29794b1f498d2a6ea
Tonyplaza <plazatony3@gmail.com>
2019-11-21 10:43:36 -0500

    Bucle en for , listbox

---
+++ AppEjerciciosBucles/frmBucleFor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppEjerciciosBucles
+{
+    public partial class frmBucleFor : Form
+    {
+        public frmBucleFor()
+        {
+            InitializeComponent();
+        }
+
+        private void lstNumeros_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnOrdenarLista_Click(object sender, EventArgs e)
+        {
+            this.lstNumeros.Sorted = true;
+        }
+
+        private void btnLlenarLista_Click(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            this.lstNumeros.Sorted = false;
+            this.lstNumeros.Items.Clear(); //vaciar lista
+            for (int i = 0; i < 10; i++)
+            {
+                int num = r.Next(0, 9);
+                this.lstNumeros.Items.Add(num);
+            }
+        }
+    }
+}
